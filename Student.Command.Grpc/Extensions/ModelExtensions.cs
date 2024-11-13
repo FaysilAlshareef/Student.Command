6a16d731ba23035558285ff9de186f47722c7d5b
Faysil Alshareef <faysilalshareef@gmail.com>
2024-11-13 15:05:11 +0200

    Add Event history

---
+++ Student.Command.Grpc/Extensions/ModelExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Student.Command.Domain.Entities;
+using Student.Command.Grpc.Protos.EventsHistory;
+
+namespace Student.Command.Grpc.Extensions
+{
+    public static class ModelExtensions
+    {
+
+        public static RepeatedField<EventMessage> ToOutputEvent(this RepeatedField<EventMessage> eventsOutput, IEnumerable<Event> events)
+        {
+            eventsOutput.AddRange(events.Select(e => new EventMessage
+            {
+                CorrelationId = e.Id.ToString(),
+                DateTime = Timestamp.FromDateTime(DateTime.SpecifyKind(e.DateTime, DateTimeKind.Utc)),
+                Data = JsonConvert.SerializeObject(((dynamic)e).Data, new StringEnumConverter()),
+                Type = e.Type.ToString(),
+                Sequence = e.Sequence,
+                Version = e.Version,
+                AggregateId = e.AggregateId.ToString(),
+                UserId = e.UserId.ToString()
+            }));
+
+            return eventsOutput;
+        }
+
+    }
+
+}
---
+++ Student.Command.Grpc/Services/EventsHistoryService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+using Student.Command.Application.Contracts.Repositories;
+using Student.Command.Grpc.Extensions;
+using Student.Command.Grpc.Protos.EventsHistory;
+
+namespace Student.Command.Grpc.Services
+{
+    public class EventsHistoryService(IUnitOfWork unitOfWork) : StudentEventsHistory.StudentEventsHistoryBase
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async override Task<Response> GetEvents(GetEventsRequest request, ServerCallContext context)
+        {
+            var events = await _unitOfWork.Events.GetAsPaginationAsync(request.CurrentPage, request.PageSize);
+
+            var response = new Response();
+
+            response.Events.ToOutputEvent(events);
+
+            return response;
+        }
+    }
+}
